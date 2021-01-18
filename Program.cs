95b8b90fc1a9961f462c2d628939bc32b7d8fda9
jeter <abc@d.e>
2021-01-18 11:29:38 +0800

    標示測驗題目
    Signed-off-by: jeter <abc@d.e>

--- Program.cs
+++ Program.cs
@@ -10,9 +10,11 @@ namespace Eight_Queens
     {
         static void Main(string[] args)
         {
+            //測驗1輸出皇后位置
             EightQueens eq1 = new EightQueens(8, PrintType.Array);
             eq1.Execute();
 
+            //測驗2輸出矩陣
             EightQueens eq2 = new EightQueens(8, PrintType.All);
             eq2.Execute();
 
